497e441bc68f52a435b78a844bd0cb79628146d6
ZTTrak <101367992+ZTTrak@users.noreply.github.com>
2026-02-17 22:46:12 -0500

    Enable password protection in config

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    forest_boyd: "sd9VEpYHG2155PADg003USKYq2sqlPXLy4LDZ3JZsQXp4bAo8A",
+    idris_reese: "ZKm8OCY1kk2OFP7mzkENA17aPmfeLuRkshR23M6dcynfWDkmQ0",
+    myles_counts: "CRVdmzuA8vKB6J0g1G3WYQGFkDWRrZmRtettCEUu6GYUWcR7tYwdzjC0vN0Dkyrs",
+    sidney_williams: "RCHqykQ03639lA984hsL",
+    guest_1: "Peko3220",
+    guest_2: "Flare5300",
+    guest_3: "FubukiXTS5000",
+    admin: "nekomataOkayu5200",
   },
 };
 
